0be0f07a47d9d176a393d408ce4f18b9ee05a46d
Tegar Arya <107928000+tegararyap@users.noreply.github.com>
2023-09-29 19:22:35 +0700

    Add files via upload

---
+++ 11. Comment/Komentar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    // Ini adalah komentar dalam 1 baris
+    /* 
+    Ini adalah komentar yang mengcakup 
+    lebih dari 1 baris
+    */
+
+    cout<<"Tegar";
+    return 0;
+}
---
+++ 11. Comment/Komentar.java
@@ -0,0 +1,10 @@
+
+public class Komentar {
+    public static void main(String[] args) {
+    // Ini adalah komentar dalam 1 baris
+    
+    System.out.println("Hello World"); /* Ini adalah komentar yang mengcakup 
+    lebih dari 1 baris */
+
+    }
+}
